778110cec09e8ce745c010d897fc73219f5902a6
Shanmukh459 <saikrrish459@gmail.com>
2024-02-17 10:43:28 -0800

    checking if search text is empty

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ searchBtn.addEventListener("click", function(e) {
     fetchMovies()
 })
 
+document.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log(e.target.dataset)
+})
+
 function handlePageSwitch(btnText) {
     if(btnText === "My Watchlist") {
         renderWatchList()
@@ -27,49 +32,67 @@ function handlePageSwitch(btnText) {
 function renderWatchList() {
     title.innerText = "My Watchlist"
     pageSwitchBtn.innerText = "Search for movies"
+    main.innerHTML = "HellO!"
 }
 
 function renderHomePage() {
     title.innerText = "Find your film"
     pageSwitchBtn.innerText = "My Watchlist"
+    main.innerHTML = getHomeHtml()
 }
 
 function fetchMovies() {
     const searchText = document.getElementById("search-text").value
     let moviesHtml = ""
-    fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchText}`)
-        .then(res => res.json())
-        .then(allMoviesData => {
-            console.log(allMoviesData)
-            allMoviesData.Search.forEach(movie => {
-                fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${movie.imdbID}`)
-                    .then(res => res.json())
-                    .then(movieData => {
-                        moviesHtml +=  `
-                        <div class="movie-card">
-                            <div>
-                                <img class="movie-poster" src=${movieData.Poster}>
-                            </div>
-                            <div class="movie-info">
-                                <div class="title-rating-sec">
-                                    <p class="movie-title">${movieData.Title}</p>
-                                    <img src="./images/star.png">
-                                    <p class="movie-rating">${movieData.imdbRating}</p>
+    if (searchText) {
+        fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchText}`)
+            .then(res => res.json())
+            .then(allMoviesData => {
+                console.log(allMoviesData)
+                allMoviesData.Search.forEach(movie => {
+                    fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${movie.imdbID}`)
+                        .then(res => res.json())
+                        .then(movieData => {
+                            moviesHtml +=  `
+                            <div class="movie-card">
+                                <div>
+                                    <img class="movie-poster" src=${movieData.Poster}>
                                 </div>
-                                <div class="duration-genre-sec">
-                                    <p class="movie-runtime">${movieData.Runtime}</p>
-                                    <p class="movie-genre">${movieData.Genre}</p>
-                                    <button class="watchlist-btn">
-                                    <img src="./images/plus.png">
-                                    <p class="watchlist-btn-text">Watchlist</p></button>
+                                <div class="movie-info">
+                                    <div class="title-rating-sec">
+                                        <p class="movie-title">${movieData.Title}</p>
+                                        <img src="./images/star.png">
+                                        <p class="movie-rating">${movieData.imdbRating}</p>
+                                    </div>
+                                    <div class="duration-genre-sec">
+                                        <p class="movie-runtime">${movieData.Runtime}</p>
+                                        <p class="movie-genre">${movieData.Genre}</p>
+                                        <button class="watchlist-btn" data-movie="${{movieData}}">
+                                        <img src="./images/plus.png" data-movie="${{movieData}}">
+                                        <p class="watchlist-btn-text" data-movie="${movieData.imdbID}">Watchlist</p></button>
+                                    </div>
+                                    <div class="plot">
+                                        <p>${movieData.Plot}
+                                    </div>
                                 </div>
-                                <div class="plot">
-                                    <p>${movieData.Plot}
-                                </div>
-                            </div>
-                        </div>`
-                        main.innerHTML = moviesHtml
+                            </div>`
+                            main.innerHTML = moviesHtml
+                    })
                 })
             })
-        })
+    }
+    else {
+        alert("Please enter text to search!!")
+    }
 }
+
+function getHomeHtml() {
+    return `
+        <div class="main-default">
+            <img class="main-img" src="./images/main.png">
+            <h2 class="default-main-text">Start exploring!</h2>
+        </div>
+    `
+}
+
+renderHomePage()
