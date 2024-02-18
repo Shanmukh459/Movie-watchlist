33d79d6948921159363a9110937f97f94d376b41
Shanmukh459 <saikrrish459@gmail.com>
2024-02-18 10:45:17 -0800

    working on watchlist screen

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const title = document.getElementById("title")
 const main = document.querySelector("main")
 const APIKEY = "46677be2"
 let allMoviesData
+let watchlistHtml = ""
 
 pageSwitchBtn.addEventListener("click", (e) => {
     e.preventDefault()
@@ -18,7 +19,37 @@ searchBtn.addEventListener("click", function(e) {
 
 document.addEventListener("click", (e) => {
     e.preventDefault()
-    console.log(e.target.dataset)
+    if (e.target.dataset.movie) {
+        const id = e.target.dataset.movie
+        fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${id}`)
+                        .then(res => res.json())
+                        .then(movieData => {
+                            // console.log(JSON.stringify(movieData))
+                            watchlistHtml +=  `
+                            <div class="movie-card">
+                                <div>
+                                    <img class="movie-poster" src=${movieData.Poster}>
+                                </div>
+                                <div class="movie-info">
+                                    <div class="title-rating-sec">
+                                        <p class="movie-title">${movieData.Title}</p>
+                                        <img src="./images/star.png">
+                                        <p class="movie-rating">${movieData.imdbRating}</p>
+                                    </div>
+                                    <div class="duration-genre-sec">
+                                        <p class="movie-runtime">${movieData.Runtime}</p>
+                                        <p class="movie-genre">${movieData.Genre}</p>
+                                        <button class="watchlist-btn" data-movie=${movieData.imdbID}>
+                                        <img src="./images/plus.png" data-movie=${movieData.imdbID}>
+                                        <p class="watchlist-btn-text" data-movie="${movieData.imdbID}">Watchlist</p></button>
+                                    </div>
+                                    <div class="plot">
+                                        <p>${movieData.Plot}
+                                    </div>
+                                </div>
+                            </div>`
+                        })
+        }
 })
 
 function handlePageSwitch(btnText) {
@@ -53,6 +84,7 @@ function fetchMovies() {
                     fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${movie.imdbID}`)
                         .then(res => res.json())
                         .then(movieData => {
+                            // console.log(JSON.stringify(movieData))
                             moviesHtml +=  `
                             <div class="movie-card">
                                 <div>
@@ -67,8 +99,8 @@ function fetchMovies() {
                                     <div class="duration-genre-sec">
                                         <p class="movie-runtime">${movieData.Runtime}</p>
                                         <p class="movie-genre">${movieData.Genre}</p>
-                                        <button class="watchlist-btn" data-movie="${{movieData}}">
-                                        <img src="./images/plus.png" data-movie="${{movieData}}">
+                                        <button class="watchlist-btn" data-movie=${movieData.imdbID}>
+                                        <img src="./images/plus.png" data-movie=${movieData.imdbID}>
                                         <p class="watchlist-btn-text" data-movie="${movieData.imdbID}">Watchlist</p></button>
                                     </div>
                                     <div class="plot">
