01fcfa6335990ed63c56f3946045087db8a8fa0a
Shanmukh459 <saikrrish459@gmail.com>
2024-02-19 11:59:46 -0800

    working code-need to fix the rerender of search results

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const title = document.getElementById("title")
 const main = document.querySelector("main")
 const APIKEY = "46677be2"
 let allMoviesData
-let watchlistHtml = localStorage.getItem("watchlistHtml") || ""
+let watchlistArr = JSON.parse(localStorage.getItem("watchlistArr")) ||getDefaultWatchlistHtml()
 
-console.log(watchlistHtml)
 
 pageSwitchBtn.addEventListener("click", (e) => {
     e.preventDefault()
-    console.log(e.target.innerText)
     handlePageSwitch(e.target.innerText)
 })
 
@@ -21,39 +19,31 @@ searchBtn.addEventListener("click", function(e) {
 
 document.addEventListener("click", (e) => {
     e.preventDefault()
-    console.log(e.target.dataset.movie)
     if (e.target.dataset.movie) {
         const id = e.target.dataset.movie
-        fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${id}`)
-            .then(res => res.json())
-            .then(movieData => {
-                // console.log(JSON.stringify(movieData))
-                watchlistHtml +=  `
-                <div class="movie-card">
-                    <div>
-                        <img class="movie-poster" src=${movieData.Poster}>
-                    </div>
-                    <div class="movie-info">
-                        <div class="title-rating-sec">
-                            <p class="movie-title">${movieData.Title}</p>
-                            <img src="./images/star.png">
-                            <p class="movie-rating">${movieData.imdbRating}</p>
-                        </div>
-                        <div class="duration-genre-sec">
-                            <p class="movie-runtime">${movieData.Runtime}</p>
-                            <p class="movie-genre">${movieData.Genre}</p>
-                            <button class="watchlist-btn" data-movie=${movieData.imdbID}>
-                            <img src="./images/minus.png" data-movie=${movieData.imdbID}>
-                            <p class="watchlist-btn-text" data-movie="${movieData.imdbID}">Remove</p></button>
-                        </div>
-                        <div class="plot">
-                            <p>${movieData.Plot}
-                        </div>
-                    </div>
-                </div>`
-                localStorage.setItem("watchlistHtml", watchlistHtml)
-            })
+        if(e.target.innerHTML === "Watchlist" || e.target.alt === "watchlist") {
+            fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${id}`)
+                .then(res => res.json())
+                .then(movieData => {
+                    watchlistArr.push({
+                        id: movieData.imdbID, 
+                        poster: movieData.Poster, 
+                        title: movieData.Title, 
+                        rating: movieData.imdbRating, 
+                        runtime: movieData.Runtime, 
+                        genre: movieData.Genre, 
+                        plot: movieData.Plot})
+                    localStorage.setItem("watchlistArr", JSON.stringify(watchlistArr))
+                })
+            }
+        else {
+            console.log("invoking remove")
+            removeMovieFromWatchlist(id)
         }
+    }
+    else if(e.target.dataset.search) {
+        renderHomePage()
+    }
 })
 
 function handlePageSwitch(btnText) {
@@ -67,7 +57,7 @@ function handlePageSwitch(btnText) {
 function renderWatchList() {
     title.innerText = "My Watchlist"
     pageSwitchBtn.innerText = "Search for movies"
-    main.innerHTML = watchlistHtml
+    main.innerHTML = getWatchlistHtml()
 }
 
 function renderHomePage() {
@@ -78,6 +68,7 @@ function renderHomePage() {
 
 function fetchMovies() {
     const searchText = document.getElementById("search-text").value
+    document.getElementById("search-text").value = ""
     let moviesHtml = ""
     if (searchText) {
         fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchText}`)
@@ -104,11 +95,12 @@ function fetchMovies() {
                                         <p class="movie-runtime">${movieData.Runtime}</p>
                                         <p class="movie-genre">${movieData.Genre}</p>
                                         <button class="watchlist-btn" data-movie=${movieData.imdbID}>
-                                        <img src="./images/plus.png" data-movie=${movieData.imdbID}>
-                                        <p class="watchlist-btn-text" data-movie="${movieData.imdbID}">Watchlist</p></button>
+                                            <img src="./images/plus.png" alt="watchlist" data-movie=${movieData.imdbID}>
+                                            <p class="watchlist-btn-text" data-movie="${movieData.imdbID}">Watchlist</p>
+                                        </button>
                                     </div>
                                     <div class="plot">
-                                        <p>${movieData.Plot}
+                                        <p>${movieData.Plot}</p>
                                     </div>
                                 </div>
                             </div>`
@@ -129,6 +121,55 @@ function getHomeHtml() {
             <h2 class="default-main-text">Start exploring!</h2>
         </div>
     `
+}
+
+function getWatchlistHtml() {
+    if(watchlistArr.length) {
+        return watchlistArr.map(movieData => {
+            return `<div class="movie-card">
+                <div>
+                    <img class="movie-poster" src=${movieData.poster}>
+                </div>
+                <div class="movie-info">
+                    <div class="title-rating-sec">
+                        <p class="movie-title">${movieData.title}</p>
+                        <img src="./images/star.png">
+                        <p class="movie-rating">${movieData.rating}</p>
+                    </div>
+                    <div class="duration-genre-sec">
+                        <p class="movie-runtime">${movieData.runtime}</p>
+                        <p class="movie-genre">${movieData.genre}</p>
+                        <button class="watchlist-btn" data-movie=${movieData.id}>
+                            <img src="./images/minus.png" alt="remove" data-movie=${movieData.id}>
+                            <p class="watchlist-btn-text" data-movie="${movieData.id}">Remove</p>
+                        </button>
+                    </div>
+                    <div class="plot">
+                        <p>${movieData.plot}</p>
+                    </div>
+                </div>
+            </div>`
+        }).join()
+    }
+    else {
+        return `
+            <div class="watchlist-default">
+                <p class="watchlist-text">Your watchlist is looking a little empty...</p>
+                <button class="watchlist-btn" data-search="search">
+                    <img src="./images/plus.png" alt="remove" data-search="search">
+                    <p class="watchlist-btn-text" data-search="search">Let's add some movies!</p>
+                </button>
+            </div>
+        `
+    }
+
+}
+
+function removeMovieFromWatchlist(id) {
+    console.log(id)
+    watchlistArr = watchlistArr.filter(movieData => movieData.id !== id)
+    localStorage.setItem("watchlistArr", JSON.stringify(watchlistArr))
+    renderWatchList()
 }
 
 renderHomePage()
